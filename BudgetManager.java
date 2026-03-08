f06fc6099ab937b8f790b14dfda2a4e6825c1b1f
kai0609-bit <kaiowada0609@gmail.com>
2026-03-08 11:48:30 +0000

    Add BudgetManager.java

---
+++ BudgetManager.java
@@ -0,0 +1,46 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional; 
+import java.util.stream.Collectors;
+
+public class BudgetManager implements Searchable{
+    static int totalTransactions = 0;
+    
+    Map<String, Transaction> transactions = new HashMap<>();
+
+    void addTransaction(Transaction transaction) {
+        transactions.put(transaction.getId(), transaction);
+
+        totalTransactions++;
+    }
+
+    public Optional<Transaction> findById(String id) {
+        return Optional.ofNullable(transactions.get(id));
+    }
+
+    void printStats() {
+        double totalAmount = transactions.values().stream()
+            .filter(t -> t.getAmount() > 0)
+            .mapToDouble(Transaction::getAmount)
+            .sum();
+        
+        double totalExpenses = transactions.values().stream()
+            .filter(t -> t.getAmount() < 0)
+            .mapToDouble(null)
+            .sum();
+        
+        List<Transaction> expenses = transactions.values().stream()
+            .filter(t -> t.getAmount() < 0)
+            .collect(Collectors.toList());
+
+        System.out.println("Total number of transactions: " + totalTransactions);
+        System.out.println("Total income: " + totalAmount);
+        System.out.println("Total expenses: " + totalExpenses);
+        System.out.println("Balance: " + totalAmount + totalExpenses);
+
+        expenses.forEach(t -> 
+            System.out.println("id: " + t.getId() + " / £" + t.getAmount())
+        );
+    }
+}
