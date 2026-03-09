9d527b57772ec4bfe150ba9536251cd4db69e8ff
kai0609-bit <kaiowada0609@gmail.com>
2026-03-09 09:14:30 +0000

    Update Main.java

--- src/main/java/com/budgettracker/Main.java
+++ src/main/java/com/budgettracker/Main.java
@@ -1,32 +1,54 @@
 package com.budgettracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         BudgetManager manager = new BudgetManager();
+        InteractiveUI ui = new InteractiveUI();
 
-        try {
-            manager.addTransaction(new Transaction("t1", "Salary", 2500.0, "salary"));
-            manager.addTransaction(new Transaction("t2", "Rent", -800.0, "rent"));
-            manager.addTransaction(new Transaction("t3", "Groceries", -150.0, "food"));
-            manager.addTransaction(new Transaction("t4", "Freelance", 500.0, "salary"));
 
-            manager.addTransaction(new Transaction("t5", "Invalid", 0.0, "other"));
-            
-        } catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
+
+        while (true) {
+            ui.showMenu();
+            int choice = Integer.parseInt(sc.nextLine());
+
+            switch(choice) {
+                // Add transaction.
+                case 1:
+                    System.out.print("Enter ID: ");
+                    String id = sc.nextLine();
+                    System.out.print("Enter description: ");
+                    String description = sc.nextLine();
+                    System.out.print("Enter amount: ");
+                    double amount = Double.parseDouble(sc.nextLine());
+                    System.out.print("Enter category: ");
+                    String category = sc.nextLine();
+
+                    manager.addTransaction(new Transaction(id, description, amount, category));
+                    break;
+
+                // View all transaction
+                case 2:
+                    manager.printAllTransaction();
+                    break;
+
+                //
+                case 3:
+                    break;
+
+                case 4:
+                    System.out.println("Goodbye!");
+                    sc.close();
+                    return;
+
+            }
+
+
         }
 
-        manager.findById("t2")
-            .ifPresent(t -> System.out.println("Found: " + t.getDescription()));
-        
-        manager.findById("t99")
-            .ifPresentOrElse(
-                t -> System.out.println("Found: " + t.getDescription()),
-                () -> System.out.println("Not Found")
-            );
 
-        System.out.println(" --- ");
-        manager.printStats();
     }
 }
